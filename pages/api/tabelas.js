1be5c41603707ee887d929546ab4745fe1969a64
jhavictor30 <jha.victor30@gmail.com>
2025-08-31 00:37:18 -0300

    Corrigir erro de sintaxe no arquivo tabelas.js da API

--- pages/api/tabelas.js
+++ pages/api/tabelas.js
@@ -1,24 +1,28 @@
-{
-  "times": [
-    {
-      "posicao": 1,
-      "nome": "Palmeiras",
-      "pontos": 45,
-      "jogos": 22,
-      "vitorias": 14,
-      "empates": 3,
-      "derrotas": 5,
-      "saldoGols": 20
-    },
-    {
-      "posicao": 2,
-      "nome": "Flamengo",
-      "pontos": 44,
-      "jogos": 22,
-      "vitorias": 13,
-      "empates": 5,
-      "derrotas": 4,
-      "saldoGols": 18
-    }
-  ]
+export default function handler(req, res) {
+  const data = {
+    "times": [
+      {
+        "posicao": 1,
+        "nome": "Palmeiras",
+        "pontos": 45,
+        "jogos": 22,
+        "vitorias": 14,
+        "empates": 3,
+        "derrotas": 5,
+        "saldoGols": 20
+      },
+      {
+        "posicao": 2,
+        "nome": "Flamengo",
+        "pontos": 44,
+        "jogos": 22,
+        "vitorias": 13,
+        "empates": 5,
+        "derrotas": 4,
+        "saldoGols": 18
+      }
+    ]
+  };
+
+  res.status(200).json(data);
 }
